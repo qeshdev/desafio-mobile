a2bc4dadd8ca46df7993c8777326a8b2555659bc
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-12-07 15:50:34 -0300

    chore: add new rules for eslint and prettier

--- project/.eslintrc.js
+++ project/.eslintrc.js
@@ -1,10 +1,17 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'prettier'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   rules: {
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
-  },
-};
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ],
+    'react-native/no-inline-styles': 'off'
+  }
+}
