f611855dbf5fbaae2d26321f779b76239babf975
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-12-08 17:27:31 -0300

    feat: ensure jsdocs to email validation

--- project/src/presentation/utils/string.ts
+++ project/src/presentation/utils/string.ts
@@ -1,3 +1,8 @@
+/**
+ * The function checks if the email is valid with a regex
+ * @param email to validate
+ * @returns true if email is valid or false if email is invalid
+ */
 export const emailValidation = (email: string): boolean => {
   if (!email) return false
   const emailRegex = /^[A-Za-z0-9_!#$%&'*+/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/
