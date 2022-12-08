4d3f582adfce16fb96ac0921af479ace338b910a
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-12-08 10:45:44 -0300

    chore: add jest config

---
+++ project/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
