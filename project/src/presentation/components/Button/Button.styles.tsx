68c7636d7bccab0826f569cfbe5c4050548ad705
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-12-08 16:45:21 -0300

    refactor: move attributes to alphabetical order

--- project/src/presentation/components/Button/Button.styles.tsx
+++ project/src/presentation/components/Button/Button.styles.tsx
@@ -2,11 +2,11 @@ import styled from 'styled-components/native'
 
 export const ButtonStyled = styled.TouchableOpacity`
   align-items: center;
-  justify-content: center;
-  width: 343px;
-  height: 48px;
   background-color: #42c1c7;
   border-radius: 8px;
+  justify-content: center;
+  height: 48px;
+  width: 343px;
 `
 
 export const Text = styled.Text`
