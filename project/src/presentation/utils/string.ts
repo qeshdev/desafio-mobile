607e8fbf65f5fb65a72b6eea97b30b59a0fca4b7
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-12-09 09:50:13 -0300

    feat: ensure password validation utils

--- project/src/presentation/utils/string.ts
+++ project/src/presentation/utils/string.ts
@@ -8,3 +8,15 @@ export const emailValidation = (email: string): boolean => {
   const emailRegex = /^[A-Za-z0-9_!#$%&'*+/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/
   return emailRegex.test(email)
 }
+
+/**
+ * The function checks if the password is valid
+ * @param password to validate
+ * @returns true if password is valid or false if password is invalid
+ * @example passwordValidation('12345') // true
+ * @example passwordValidation('1234') // false
+ */
+export const passwordValidation = (password: string): boolean => {
+  if (!password) return false
+  return password.length >= 5
+}
