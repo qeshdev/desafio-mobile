c47d50815e8e4e74d969e56b23a4894d71a3c143
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-12-09 09:50:37 -0300

    test: ensure password validation utils

--- project/src/presentation/utils/string.spec.ts
+++ project/src/presentation/utils/string.spec.ts
@@ -1,11 +1,17 @@
 import { faker } from '@faker-js/faker'
 
-import { emailValidation } from './string'
+import { emailValidation, passwordValidation } from './string'
 
 describe('presentation/utils/string', () => {
   test('#emailValidation works correctly', () => {
     expect(emailValidation('')).toBeFalsy()
     expect(emailValidation('invalid_email')).toBeFalsy()
     expect(emailValidation(faker.internet.email())).toBeTruthy()
+  })
+
+  test('#passwordValidation works correctly', () => {
+    expect(passwordValidation('')).toBeFalsy()
+    expect(passwordValidation('1234')).toBeFalsy()
+    expect(passwordValidation('12345')).toBeTruthy()
   })
 })
